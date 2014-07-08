0600d01a2e0b0a22be0c25bac1491f3341363f84
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-07-08 22:26:24 +0200

    Test wyszukowania wszystkich rekordów, który przechodzi, jeśli metoda findAll nie zwraca nulla.

--- akademik/src/test/java/org/wojtekz/akademik/dao/StudentDaoImplTest.java
+++ akademik/src/test/java/org/wojtekz/akademik/dao/StudentDaoImplTest.java
@@ -1,14 +1,23 @@
 package org.wojtekz.akademik.dao;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.wojtekz.akademik.entity.Student;
 
 public class StudentDaoImplTest {
+	private Student student;
+	private StudentDao studentDao = new StudentDaoImpl();
+	
+	@Before
+	public void before() {
+		student = new Student();
+		student.setId(1);
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void findAllTest() {
+		Assert.assertNotNull(studentDao.findAll());
 	}
 
 }
