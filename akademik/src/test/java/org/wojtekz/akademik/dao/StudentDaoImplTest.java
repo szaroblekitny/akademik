630de63aa32fbf2de419e8e03e9b3b126d1e6029
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-07-08 21:54:11 +0200

    Bardzo wstępny test implementacji StudentDAO

---
+++ akademik/src/test/java/org/wojtekz/akademik/dao/StudentDaoImplTest.java
@@ -0,0 +1,14 @@
+package org.wojtekz.akademik.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentDaoImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
