4ff2d1f10bd47d96e947e4d8683536aba6421b62
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-07-14 21:34:18 +0200

    Jakieś metody zaimplemenowane

--- akademik/src/main/java/org/wojtekz/akademik/dao/StudentDaoImpl.java
+++ akademik/src/main/java/org/wojtekz/akademik/dao/StudentDaoImpl.java
@@ -16,110 +16,94 @@ public class StudentDaoImpl implements StudentDao {
 	@PersistenceContext
 	private EntityManager entityManager;
 
-	@Override
 	public void deleteAllInBatch() {
-		deleteAll();
+		deleteAll();		
 	}
 
-	@Override
-	public void deleteInBatch(Iterable<Student> arg0) {
+	public void deleteInBatch(Iterable<Student> entities) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
-	@Override
 	public List<Student> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Student> listaWyn = entityManager.createQuery("from student", Student.class).getResultList();
+		return listaWyn;
 	}
 
-	@Override
 	public List<Student> findAll(Sort sort) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<Student> findAll(Iterable<Long> ids) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void flush() {
-		// TODO Auto-generated method stub
-
+		entityManager.flush();		
 	}
 
-	@Override
 	public Student getOne(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public <S extends Student> List<S> save(Iterable<S> entities) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public <S extends Student> S saveAndFlush(S entity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Page<Student> findAll(Pageable pageable) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public long count() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public void delete(Long id) {
 		Student student = findOne(id);
 		delete(student);
 	}
 
-	@Override
 	public void delete(Student entity) {
 		entityManager.remove(entity);
 	}
 
-	@Override
 	public void delete(Iterable<? extends Student> entities) {
 		for (Student student: entities) {
 			delete(student);
 		}
-
+		
 	}
 
-	@Override
 	public void deleteAll() {
 		// TODO Auto-generated method stub
+		
 	}
 
-	@Override
 	public boolean exists(Long id) {
 		return entityManager.contains(findOne(id));
 	}
 
-	@Override
 	public Student findOne(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public <S extends Student> S save(S entity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }
