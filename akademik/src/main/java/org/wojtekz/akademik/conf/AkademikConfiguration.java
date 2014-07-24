a621ff91f3f3567b27ac3682b9e5aa3aa7c1bd94
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-07-24 18:21:23 +0200

    Do konfiguracji dodany LocalEntityManagerFactoryBean

--- akademik/src/main/java/org/wojtekz/akademik/conf/AkademikConfiguration.java
+++ akademik/src/main/java/org/wojtekz/akademik/conf/AkademikConfiguration.java
@@ -2,14 +2,32 @@ package org.wojtekz.akademik.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.jpa.JpaDialect;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.wojtekz.akademik.dao.StudentDao;
 import org.wojtekz.akademik.dao.StudentDaoImpl;
 
 @Configuration
+@ComponentScan(basePackages="org.wojtekz.akademik.dao")
 public class AkademikConfiguration {
 	@Bean
 	StudentDao studentDao() {
 		return new StudentDaoImpl();
+	}
+	
+	@Bean
+	LocalEntityManagerFactoryBean entityManagerFactory() {
+		JpaDialect jpaDialect = new HibernateJpaDialect();
+		JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		LocalEntityManagerFactoryBean localEntityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+		
+		localEntityManagerFactoryBean.setJpaDialect(jpaDialect);
+		localEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+		return localEntityManagerFactoryBean; 
 	}
 	
 }
