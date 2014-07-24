b8027d8f178bf01276eb1dc2cb4666ec745f5a22
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-07-24 21:02:01 +0200

    poprawione logowanie i dodany komentarz

--- akademik/src/test/java/org/wojtekz/akademik/dao/StudentDaoImplTest.java
+++ akademik/src/test/java/org/wojtekz/akademik/dao/StudentDaoImplTest.java
@@ -24,14 +24,20 @@ public class StudentDaoImplTest {
 	
 	@Before
 	public void before() {
-		logg.debug("before method fired");
+		logg.debug("----->>> before method fired");
 		student = new Student();
 		student.setId(1);
 	}
 
+	/**
+	 * Metoda testuje tylko, czy zwracany obiekt nie jest nulem. W³aœciwie s³u¿y
+	 * do przetestowania ca³ego ci¹gu persystencji dla klasy Student. W tym teœcie
+	 * ¿adnymi danymi siê nie przejmujemy. Tak napradwdê sprawdzane jest, czy cokolwiek
+	 * jest zwracane po przejœciu wszystkich stopni po³¹czenia z baz¹ danych. 
+	 */
 	@Test
 	public void findAllTest() {
-		logg.debug("findAllTest method fired");
+		logg.debug("----->>> findAllTest method fired");
 		Assert.assertNotNull(studentDao.findAll());
 	}
 
