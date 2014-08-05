13dc53fc4a5adc632a8c26a98f664dcd57340933
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-08-05 21:34:14 +0200

    Konfiguracja transaction managera przeniesiona do pliku config_dao.xml

--- akademik/src/main/java/org/wojtekz/akademik/conf/AkademikConfiguration.java
+++ akademik/src/main/java/org/wojtekz/akademik/conf/AkademikConfiguration.java
@@ -4,49 +4,26 @@ import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.orm.jpa.JpaDialect;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.jta.JtaTransactionManager;
+// import org.wojtekz.akademik.dao.StudentDao;
+// import org.wojtekz.akademik.dao.StudentDaoImpl;
 
 @Configuration
-@EnableTransactionManagement
 @ComponentScan(basePackages="org.wojtekz.akademik.dao")
+@ImportResource("classpath:config_dao.xml")
 public class AkademikConfiguration {
 	private static Logger logg = Logger.getLogger(AkademikConfiguration.class.getName());
-
-	@Bean
-	JtaTransactionManager transactionManager() {
-		logg.debug("----->>> transactionManager instance ----");
-		JtaTransactionManager txManager = new JtaTransactionManager();
-		/*JndiTemplate template = new JndiTemplate();
-		Properties jndiEnvironment = new Properties();
-		jndiEnvironment.setProperty("hibernate.connection.url", "jdbc:hsqldb:mem:app-db");
-		jndiEnvironment.setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbcDriver");
-		jndiEnvironment.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-		jndiEnvironment.setProperty("hibernate.connection.username", "sa");
-		jndiEnvironment.setProperty("hibernate.connection.password", "");
-		jndiEnvironment.setProperty("hibernate.generate_statistics", "true");
-		jndiEnvironment.setProperty("hibernate.hbm2ddl.auto", "update");
-		// jndiEnvironment.setProperty("hibernate.default_batch_fetch_size", "2");
-		jndiEnvironment.setProperty("hibernate.show_sql", "true");
-		jndiEnvironment.setProperty("hibernate.format_sql", "true");
-		jndiEnvironment.setProperty("hibernate.connection.charSet", "UTF-8");
-		
-		template.setEnvironment(jndiEnvironment);
-		
-		txManager.setJndiTemplate(template);
-		UserTransaction userTransaction = new User;
-		txManager.setUserTransaction(userTransaction);*/
-		
-		// txManager.
-		
-		return txManager;
-	}
 	
+	/*@Bean
+	StudentDao studentDao() {
+		return new StudentDaoImpl();
+	}*/
+
 	@Bean
 	LocalEntityManagerFactoryBean entityManagerFactory() {
 		logg.debug("----->>> entityManagerFactory instance ----");
