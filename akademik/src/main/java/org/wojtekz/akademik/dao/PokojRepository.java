cfaa1bfce1579dc052921cda08497e1ffba4b4b7
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-08-12 19:39:54 +0200

    Zmiana nazwy z PokojDao na PokojRepository

--- akademik/src/main/java/org/wojtekz/akademik/dao/PokojRepository.java
+++ akademik/src/main/java/org/wojtekz/akademik/dao/PokojRepository.java
@@ -3,6 +3,6 @@ package org.wojtekz.akademik.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.wojtekz.akademik.entity.Pokoj;
 
-public interface PokojDao extends JpaRepository<Pokoj, Long> {
+public interface PokojRepository extends JpaRepository<Pokoj, Long> {
 
 }
