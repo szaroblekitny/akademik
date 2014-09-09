9e61c3460ab0d7f670564f2b8e09b99d8f770ebf
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-09-09 21:09:52 +0200

    Dodane puste metody statyczne dla skupienia uwagi na małych kawałkach.

--- akademik/src/main/java/org/wojtekz/akademik/core/AkademikApplication.java
+++ akademik/src/main/java/org/wojtekz/akademik/core/AkademikApplication.java
@@ -1,11 +1,16 @@
 package org.wojtekz.akademik.core;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.wojtekz.akademik.conf.AkademikConfiguration;
 import org.wojtekz.akademik.entity.Pokoj;
+import org.wojtekz.akademik.entity.Student;
 
 public class AkademikApplication {
 	private static Logger logg = Logger.getLogger(AkademikApplication.class.getName());
@@ -32,5 +37,28 @@ public class AkademikApplication {
 		((AbstractApplicationContext) applicationContext).close();
 		logg.info("----->>> End of AkademikApplication");
 	}
+	
+	public static void pobierzPokoje(BufferedReader reader) {
+		// TODO wczytywanie listy pokoi (do bazy danych)
+	}
+	
+	public static void pobierzStudentow(BufferedReader reader) {
+		// TODO wczytywanie listy studentow
+	}
+	
+	public static void zakwateruj() {
+		// TODO dorobiæ kwaterunek
+	}
+	
+	public static void podajStanAkademika(BufferedWriter writer) {
+		// TODO wypisanie listy pokoi z zakwaterowanymi studentami
+	}
+	
+	public static void zapiszPokojeDoBufora(BufferedWriter writer, List<Pokoj> listaPokoi) {
+		// TODO zrzucenie listy pokoi do bufora (plikowego)
+	}
 
+	public static void zapiszStudentowDoBufora(BufferedWriter writer, List<Student> listaStudentow) {
+		// TODO zrzucenie listy studentow do bufora (plikowego)
+	}
 }
