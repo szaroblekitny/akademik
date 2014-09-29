2752ac10a2a30d01fe2b7fef30de121756a39d80
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-09-29 20:14:58 +0200

    Dodany test pobierania studentów i zapisu do bazy.

--- akademik/src/test/java/org/wojtekz/akademik/core/AkademikApplicationTest.java
+++ akademik/src/test/java/org/wojtekz/akademik/core/AkademikApplicationTest.java
@@ -21,7 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.wojtekz.akademik.conf.AkademikConfiguration;
+import org.wojtekz.akademik.entity.Plec;
 import org.wojtekz.akademik.entity.Pokoj;
+import org.wojtekz.akademik.entity.Student;
 
 /**
  * G³ówny test g³ównej klasy naszego Akademika.
@@ -33,11 +35,15 @@ import org.wojtekz.akademik.entity.Pokoj;
 @ContextConfiguration(classes = {AkademikConfiguration.class})
 public class AkademikApplicationTest {
 	private static Logger logg = Logger.getLogger(AkademikApplicationTest.class.getName());
-	private static Path path;
+	private static Path pathPokoi;
+	private static Path pathStudentow;
 	private static Charset charset = StandardCharsets.UTF_8;
 	
 	@Autowired
 	PokojService pokService;
+	
+	@Autowired
+	StudentService studService;
 	
 	@Autowired
 	Plikowanie plikowanie;
@@ -62,25 +68,63 @@ public class AkademikApplicationTest {
 		pokoje.add(pok1);
 		pokoje.add(pok2);
 		
+		// Studenci:
+		Student kowalski = new Student();
+		kowalski.setId(1);
+		kowalski.setImie("Jan");
+		kowalski.setNazwisko("Kowalski");
+		kowalski.setPlec(Plec.Mezczyzna);
+		
+		Student kowalska = new Student();
+		kowalska.setId(2);
+		kowalska.setImie("Janina");
+		kowalska.setNazwisko("Kowalska");
+		kowalska.setPlec(Plec.Kobieta);
+		
+		Student malinowski = new Student();
+		malinowski.setId(3);
+		malinowski.setImie("Adam");
+		malinowski.setNazwisko("Malinowski");
+		malinowski.setPlec(Plec.Mezczyzna);
+		
+		Student nowak = new Student();
+		nowak.setId(4);
+		nowak.setImie("Miros³aw");
+		nowak.setNazwisko("Nowak");
+		nowak.setPlec(Plec.Mezczyzna);
+		
+		List<Student> studenci = new ArrayList<Student>();
+		studenci.add(kowalska);
+		studenci.add(kowalski);
+		studenci.add(nowak);
+		studenci.add(malinowski);
+		
+		
 		// saveObjectList(BufferedWriter writer, List<T> list)
 		
-		path = FileSystems.getDefault().getPath("pokoje_test_appl.xml");
+		pathPokoi = FileSystems.getDefault().getPath("pokoje_test_appl.xml");
+		pathStudentow = FileSystems.getDefault().getPath("studenci_test_appl.xml");
 		try {
-			BufferedWriter bufWriter = Files.newBufferedWriter(path, charset);
+			BufferedWriter bufWriter = Files.newBufferedWriter(pathPokoi, charset);
 			logg.debug("----->>> " + pokoje.toString());
 			plikowanie.saveObjectList(bufWriter, pokoje);
 			bufWriter.close();
+			
+			bufWriter = Files.newBufferedWriter(pathStudentow, charset);
+			logg.debug("----->>> " + studenci.toString());
+			plikowanie.saveObjectList(bufWriter, studenci);
+			bufWriter.close();
 		} catch (IOException ee) {
-			logg.error("----- ERROR >> B³¹d tworzenia pliku z pokojami");
+			logg.error("----- ERROR >> B³¹d tworzenia plików");
 			Assert.fail("Exception: " + ee.getMessage());
 		}
 		
-		logg.debug("----->>> przedTest mamy plik z pokojami");
+		logg.debug("----->>> przedTest mamy pliki z danymi");
 		
-		// kasujemy pokoje dla czystoœci
+		// kasujemy dla czystoœci
 		pokService.deleteAll();
-		
-		
+		studService.deleteAll();
+		logg.debug("----->>> przedTest koniec");
 		
 	}
 
@@ -92,7 +136,7 @@ public class AkademikApplicationTest {
 		logg.debug("----->>> testPobierzPokoje starts");
 		
 		try {
-			BufferedReader reader = Files.newBufferedReader(path, charset);
+			BufferedReader reader = Files.newBufferedReader(pathPokoi, charset);
 			akademik.pobierzPokoje(reader);
 			reader.close();
 			
@@ -105,6 +149,26 @@ public class AkademikApplicationTest {
 		}
 		
 		logg.debug("----->>> testPobierzPokoje finish");
+	}
+	
+	@Test
+	public void testPobierzStudentow () {
+		logg.debug("----->>> testPobierzStudentow starts");
+		
+		try {
+			BufferedReader reader = Files.newBufferedReader(pathStudentow, charset);
+			akademik.pobierzStudentow(reader);
+			reader.close();
+			
+			Student malinowskiZBazy = studService.findByName("Malinowski");
+			Assert.assertEquals(3, malinowskiZBazy.getId());
+			
+		} catch (IOException ee) {
+			logg.error("----- ERROR >> B³¹d odczytu pliku ze studentami");
+			Assert.fail("Exception: " + ee.getMessage());
+		}
+		
+		logg.debug("----->>> testPobierzStudentow finish");
 	}
 
 }
