e0d73307228ad4a64cc3ca3e621ca3fb6893c019
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-09-29 20:26:08 +0200

    Ta klasa reprezentuje powiązanie pomiędzy pokojami a studentami

---
+++ akademik/src/main/java/org/wojtekz/akademik/entity/Kwaterunek.java
@@ -0,0 +1,57 @@
+package org.wojtekz.akademik.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Ta klasa reprezentuje powi¹zanie pomiêdzy pokojami a studentami.
+ * Oprócz swojego identyfikatora ma identyfikator pokoju i studenta
+ * mieszkaj¹cego w nim.
+ * 
+ * @author Wojtek
+ *
+ */
+@Entity
+public class Kwaterunek {
+	
+	@Id
+	private long id;
+	private long student;
+	private long pokoj;
+	
+	public Kwaterunek() {
+		
+	}
+	
+	public Kwaterunek(long id, long student, long pokoj) {
+		this.id = id;
+		this.student = student;
+		this.pokoj = pokoj;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getStudent() {
+		return student;
+	}
+	public void setStudent(long student) {
+		this.student = student;
+	}
+	public long getPokoj() {
+		return pokoj;
+	}
+	public void setPokoj(long pokoj) {
+		this.pokoj = pokoj;
+	}
+	
+	@Override
+	public String toString() {
+		return "Kwaterunek [id=" + id + ", student=" + student + ", pokoj=" + pokoj + "]";
+	}
+
+}
