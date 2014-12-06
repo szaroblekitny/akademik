c1a21efd0a6a125a7d759bda72631d3ef61b49f6
Wojciech Zaręba <szaroblekitny@gmail.com>
2014-12-06 18:27:52 +0100

    Dodane opisy metod

--- akademik/src/main/java/org/wojtekz/akademik/core/PokojService.java
+++ akademik/src/main/java/org/wojtekz/akademik/core/PokojService.java
@@ -20,11 +20,43 @@ import org.wojtekz.akademik.entity.Pokoj;
  *
  */
 public interface PokojService {
+	/**
+	 * Zapisuje pokój w bazie danych.
+	 * @param pokoj Pokoj
+	 */
 	public void save(Pokoj pokoj);
+	
+	/**
+	 * Pobiera wszystkie pokoje z bazy danych.
+	 * @return lista pokoi
+	 */
 	public List<Pokoj> listAll();
+	
+	/**
+	 * Wyszukuje w bazie pokój o podanym numerze.
+	 * 
+	 * @param numerPokoju jako String
+	 * @return Pokoj
+	 */
 	public Pokoj findByNumber(String numerPokoju);
+	
+	/**
+	 * Podaje liczbê pokoi zapisanych z bazie danych.
+	 * 
+	 * @return liczba pokoi
+	 */
 	public long ilePokoi();
+	
+	/**
+	 * Kasuje ca³¹ tabelê pokoi.
+	 */
 	public void deleteAll();
+	
+	/**
+	 * Kasuje pokój o wybranym numerze.
+	 * 
+	 * @param numerPokoju jako String (to nie jest id pokoju)
+	 */
 	public void deleteByNumber(String numerPokoju);
 
 }
