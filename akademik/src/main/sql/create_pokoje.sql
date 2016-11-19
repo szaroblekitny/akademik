f8812a934c23b898194a73b9310863993c0893a3
Wojciech Zaręba <szaroblekitny@gmail.com>
2016-11-19 18:24:08 +0100

    Dodane pliki sql do tworzenia tabel pokoje i studenci

---
+++ akademik/src/main/sql/create_pokoje.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pokoje (
+    id           integer CONSTRAINT pokojkey PRIMARY KEY,
+    numerPokoju  varchar(5),
+    liczbaMiejsc integer
+);
---
+++ akademik/src/main/sql/create_studenci.sql
@@ -0,0 +1,7 @@
+CREATE TABLE studenci (
+    id           integer CONSTRAINT studentkey PRIMARY KEY,
+    imie         varchar(100),
+    nazwisko     varchar(100),
+    plec         varchar(100)
+);
+
