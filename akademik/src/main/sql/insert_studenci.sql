9a7357a4e38b3e435b99baf38f67456e4e143b7e
Wojciech Zaręba <szaroblekitny@gmail.com>
2016-11-19 19:13:07 +0100

    Przykładowe dane dla studentów

---
+++ akademik/src/main/sql/insert_studenci.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.studenci VALUES (1, 'Jan', 'Kowalski', 'Mezczyzna');
+INSERT INTO public.studenci VALUES (2, 'Joanna', 'Kowalska', 'Kobieta');
+INSERT INTO public.studenci VALUES (3, 'Janusz', 'Kowal', 'Mezczyzna');
+INSERT INTO public.studenci VALUES (4, 'Ireneusz', 'Kapucyn', 'Mezczyzna');
+INSERT INTO public.studenci VALUES (5, 'Jeremi', 'Kulka', 'Mezczyzna');
+INSERT INTO public.studenci VALUES (6, 'Justyn', 'Kaleta', 'Mezczyzna');
+INSERT INTO public.studenci VALUES (7, 'Janina', 'Kaptur', 'Kobieta');
+commit;
