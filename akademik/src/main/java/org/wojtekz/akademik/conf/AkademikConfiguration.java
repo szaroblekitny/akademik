7fb7949f978821d848a6ae1246459169cdace1ff
Wojciech Zaręba <szaroblekitny@gmail.com>
2017-01-19 20:31:18 +0100

    JtaTransactionManager nie wymaga podania EntityManagerFactory

--- akademik/src/main/java/org/wojtekz/akademik/conf/AkademikConfiguration.java
+++ akademik/src/main/java/org/wojtekz/akademik/conf/AkademikConfiguration.java
@@ -1,17 +1,12 @@
 package org.wojtekz.akademik.conf;
 
-import javax.persistence.EntityManagerFactory;
-
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -48,21 +43,15 @@ public class AkademikConfiguration {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		
 		em.setPackagesToScan("org.wojtekz.akademik.entity");
-
-//		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-//		em.setJpaVendorAdapter(vendorAdapter);
-//		em.setJpaDialect(new HibernateJpaDialect());
-		// em.setJpaProperties(hibernateProperties());
 		em.setPersistenceUnitName("unitPU");
 
 		return em;
 	}
 	
 	@Bean
-	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+	public PlatformTransactionManager transactionManager() {
 		logg.debug("----->>> PlatformTransactionManager bean configuration");
 		JtaTransactionManager transactionManager = new JtaTransactionManager();
-		// transactionManager.setEntityManagerFactory(emf);
 
 		return transactionManager;
 	}
