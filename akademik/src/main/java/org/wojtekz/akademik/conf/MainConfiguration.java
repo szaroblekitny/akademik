19af3802a31c4f36f2212c792412921d1887d24a
Wojciech Zaręba <wzareba@wojtek-Z170.home>
2020-03-18 22:28:18 +0100

    Dodany Atomikos JTA

--- akademik/src/main/java/org/wojtekz/akademik/conf/MainConfiguration.java
+++ akademik/src/main/java/org/wojtekz/akademik/conf/MainConfiguration.java
@@ -1,6 +1,10 @@
 package org.wojtekz.akademik.conf;
 
 import org.apache.logging.log4j.Logger;
+
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
 import org.apache.logging.log4j.LogManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +14,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
+
 /**
  * Ta klasa konfiguruje "produkcyjne" (nietestowe) połączenie z bazą danych.
  * Do testów konfiguracja będzie brana z TestConfiguration.
  * 
- * @author wojtek
+ * @author Wojciech Zaręba
  *
  */
 @Configuration
@@ -35,10 +42,35 @@ public class MainConfiguration {
 	}
 	
 	@Bean
+	public UserTransactionManager atomikosTransactionManager() {
+		logg.debug("----->>> UserTransactionManager bean configuration");
+		UserTransactionManager userTransactionManager = new UserTransactionManager();
+		
+		// when close is called, should we force transactions to terminate, or not?
+		userTransactionManager.setForceShutdown(false);
+		
+		return userTransactionManager;
+	}
+	
+	@Bean
+	UserTransaction userTransaction() {
+		logg.debug("----->>> UserTransaction bean configuration");
+		UserTransaction userTransaction = new UserTransactionImp();
+		try {
+			userTransaction.setTransactionTimeout(300);
+		} catch (SystemException se) {
+			logg.error("----->>> STRASZNY BŁĄD SERWISU TRANSAKCJI", se);
+		}
+		return userTransaction;
+	}
+	
+	@Bean
 	public PlatformTransactionManager transactionManager() {
 		logg.debug("----->>> PlatformTransactionManager bean configuration");
 		JtaTransactionManager transactionManager = new JtaTransactionManager();
-
+		transactionManager.setUserTransaction(userTransaction());
+		transactionManager.setTransactionManager(atomikosTransactionManager());
+		
 		return transactionManager;
 	}
 
