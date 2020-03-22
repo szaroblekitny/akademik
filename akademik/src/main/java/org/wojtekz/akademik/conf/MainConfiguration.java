6d0ce7f4a739ab96d8d43439b727a972e73d845b
Wojciech Zaręba <wzareba@wojtek-Z170.home>
2020-03-22 21:12:01 +0100

    Działająca konfiguracja persystencji i transakcyjności

--- akademik/src/main/java/org/wojtekz/akademik/conf/MainConfiguration.java
+++ akademik/src/main/java/org/wojtekz/akademik/conf/MainConfiguration.java
@@ -2,6 +2,10 @@ package org.wojtekz.akademik.conf;
 
 import org.apache.logging.log4j.Logger;
 
+import java.util.Properties;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 
@@ -9,7 +13,10 @@ import org.apache.logging.log4j.LogManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
@@ -30,21 +37,58 @@ import com.atomikos.icatch.jta.UserTransactionManager;
 public class MainConfiguration {
 	private static Logger logg = LogManager.getLogger();
 	
-	@Bean
+	/**
+	 * Właściwości dla Hibernejta. W zasadzie powinny być w pliku, 
+	 * ale tu jest na razie łatwiej.
+	 * 
+	 * @return właściwosci sterujące Hibernejtem
+	 */
+	Properties hibernatelProperties() {
+	    Properties properties = new Properties();
+	    properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+	    properties.setProperty("hibernate.generate_statistics", "false");
+	    properties.setProperty("hibernate.default_batch_fetch_size", "2");
+	    properties.setProperty("hibernate.show_sql", "true");
+	    properties.setProperty("hibernate.format_sql", "true");
+	    properties.setProperty("hibernate.use_sql_comments", "true");
+	    
+	    return properties;
+	}
+	
+    @Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		logg.debug("----->>> LocalContainerEntityManagerFactoryBean bean configuration");
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		
 		em.setPackagesToScan("org.wojtekz.akademik.entity");
-		em.setPersistenceUnitName("unitPU");
+		try {
+			em.setJtaDataSource(dataSource());
+		} catch (NamingException ne) {
+			logg.error("=====>> Błąd szukania bazy danych!");
+		}
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		em.setJpaProperties(hibernatelProperties());
 
 		return em;
 	}
+    
+    @Bean
+    public DataSource dataSource() throws NamingException {
+    	logg.debug("----->>> DataSource bean configuration");
+        return (DataSource) new JndiTemplate().lookup("java:comp/env/jdbc/akademik");
+    }
 	
 	@Bean
 	public UserTransactionManager atomikosTransactionManager() {
 		logg.debug("----->>> UserTransactionManager bean configuration");
 		UserTransactionManager userTransactionManager = new UserTransactionManager();
+		try {
+			userTransactionManager.init();
+		} catch (SystemException se) {
+			logg.error("----->>> STRASZNY BŁĄD inita UserTransactionManagera", se);
+		}
 		
 		// when close is called, should we force transactions to terminate, or not?
 		userTransactionManager.setForceShutdown(false);
@@ -67,6 +111,7 @@ public class MainConfiguration {
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 		logg.debug("----->>> PlatformTransactionManager bean configuration");
+		
 		JtaTransactionManager transactionManager = new JtaTransactionManager();
 		transactionManager.setUserTransaction(userTransaction());
 		transactionManager.setTransactionManager(atomikosTransactionManager());
