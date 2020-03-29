315de157c8f9f587df64b1e0e5b41d616d0f8d93
Wojciech Zaręba <wzareba@wojtek-Z170.home>
2020-03-29 18:11:14 +0200

    Zrąb aplikacji: tabele, dane, stronki i niektóre beany stronek

---
+++ akademik/src/main/sql/insert_pokoje.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.pokoj VALUES (1, '101', 2);
+INSERT INTO public.pokoj VALUES (2, '102', 2);
+INSERT INTO public.pokoj VALUES (3, '103', 2);
+INSERT INTO public.pokoj VALUES (4, '201', 3);
+
