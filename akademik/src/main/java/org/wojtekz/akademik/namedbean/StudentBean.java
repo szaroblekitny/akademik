546c61459fb63d044a60244da661b3c9b9d5fab4
Wojciech Zaręba <wzareba@wojtek-Z170.home>
2020-04-01 22:08:11 +0200

    Dodana ramka ładnie wyświetlająca studentów

--- akademik/src/main/java/org/wojtekz/akademik/namedbean/StudentBean.java
+++ akademik/src/main/java/org/wojtekz/akademik/namedbean/StudentBean.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +24,28 @@ import org.wojtekz.akademik.services.StudentService;
 @Scope("session")
 public class StudentBean implements Serializable {
 	private static final long serialVersionUID = 297092190215801549L;
-
 	private static Logger logg = LogManager.getLogger();
+
+	private List<Student> studenci;
 	
 	@Autowired
 	private transient StudentService studentServ;
 	
+	@PostConstruct
+    public void init() {
+		logg.debug("------> PostConstruct StudentBean");
+		studenci = studentServ.listAll();
+    }
+	
+	/**
+	 * Lista studentów dla PrimeFaces (jest deczko prościej).
+	 * 
+	 * @return po prostu wszyscy studenci jako lista
+	 */
+	public List<Student> getStudenci() {
+		return studenci;
+	}
+
 	/**
 	 * Pobiera listę studentów z bazy i przekształca na Stringi czytelne dla strony web.
 	 * 
