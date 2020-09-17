cabfcf702cdce729046faeb7cc79add65afa03db
Wojciech Zaręba <szaroblekitny@gmail.com>
2020-09-17 22:14:05 +0200

    #20 Przed wywołaniem procedury kwaterowania w guziku jest wykonywane kasowanie pola zakwateruj u wszystkich studentów.

--- akademik/src/main/java/org/wojtekz/akademik/namedbean/ZakwaterujBean.java
+++ akademik/src/main/java/org/wojtekz/akademik/namedbean/ZakwaterujBean.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.wojtekz.akademik.core.Akademik;
+import org.wojtekz.akademik.entity.Student;
+import org.wojtekz.akademik.repo.StudentRepository;
 
 /**
  * Bean do obsługi kwaterowania - wypełniania tabelki Kwaterunek.
@@ -23,6 +25,7 @@ public class ZakwaterujBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private static Logger logg = LogManager.getLogger();
 	
+	private StudentRepository studentRepository;
 	private transient Messagesy komunikaty;
 	private transient Akademik akademik;
 	
@@ -49,6 +52,16 @@ public class ZakwaterujBean implements Serializable {
 	}
 	
 	/**
+	 * Wstawia klasę repozytorium studentów do tego komponentu.
+	 * 
+	 * @param studentRepository repozytorium studentów
+	 */
+	@Autowired
+	public void setStudentRepository(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+	
+	/**
 	 * Pobiera klasę {@link org.wojtekz.akademik.core.Akademik} z tego komponentu.
 	 * 
 	 * @return zwracana klasa
@@ -59,11 +72,20 @@ public class ZakwaterujBean implements Serializable {
 	
 	/**
 	 * Wywołuje metodę {@link org.wojtekz.akademik.core.Akademik#zakwateruj}
-	 * i wyświetla stosowny komunikat.
+	 * i wyświetla stosowny komunikat. Najpierw jednak usuwa znaczniki
+	 * kwaterowania w tabeli studentów.
 	 * 
 	 */
 	public void zakwateruj() {
 		logg.debug("------> zakwateruj w Beanie");
+		// rozkwaterowanie - usunięcie wpisów w polu zakwaterowani tabeli studentów
+		
+		for (Student student : studentRepository.findAll()) {
+			student.setPokoj(null);
+    		studentRepository.save(student);
+    	}
+		
+		
 		boolean ok = akademik.zakwateruj();
 		komunikaty.addMessage("Kwaterowanie", ok ? "OK" : "Nie poszło");
 	}
