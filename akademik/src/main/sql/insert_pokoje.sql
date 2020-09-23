638e3ba6f51fffe87cfc3d50fc1983b1b42d6374
Wojciech Zaręba <szaroblekitny@gmail.com>
2020-09-23 16:49:02 +0200

    #12 Poprawione dane wstawiane pierwotnie do bazy

--- akademik/src/main/sql/insert_pokoje.sql
+++ akademik/src/main/sql/insert_pokoje.sql
@@ -1,5 +1,5 @@
-INSERT INTO public.pokoj VALUES (1, '101', 2);
+INSERT INTO public.pokoj VALUES (1, '101', 1);
 INSERT INTO public.pokoj VALUES (2, '102', 2);
-INSERT INTO public.pokoj VALUES (3, '103', 2);
-INSERT INTO public.pokoj VALUES (4, '201', 3);
+INSERT INTO public.pokoj VALUES (3, '103', 3);
+INSERT INTO public.pokoj VALUES (4, '104', 4);
 
--- akademik/src/main/sql/insert_studenci.sql
+++ akademik/src/main/sql/insert_studenci.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.student VALUES (1, 'Jan', 'Kowalski', 'Mezczyzna');
-INSERT INTO public.student VALUES (2, 'Joanna', 'Kowalska', 'Kobieta');
-INSERT INTO public.student VALUES (3, 'Janusz', 'Kowal', 'Mezczyzna');
-INSERT INTO public.student VALUES (4, 'Ireneusz', 'Kapucyn', 'Mezczyzna');
-INSERT INTO public.student VALUES (5, 'Jeremi', 'Kulka', 'Mezczyzna');
-INSERT INTO public.student VALUES (6, 'Justyn', 'Kaleta', 'Mezczyzna');
-INSERT INTO public.student VALUES (7, 'Janina', 'Kaptur', 'Kobieta');
+INSERT INTO public.student VALUES (1, 'Jan', 'Kowalski', 'Mezczyzna', null);
+INSERT INTO public.student VALUES (2, 'Joanna', 'Kowalska', 'Kobieta', null);
+INSERT INTO public.student VALUES (3, 'Janusz', 'Kowal', 'Mezczyzna', null);
+INSERT INTO public.student VALUES (4, 'Ireneusz', 'Kapucyn', 'Mezczyzna', null);
+INSERT INTO public.student VALUES (5, 'Jeremi', 'Kulka', 'Mezczyzna', null);
+INSERT INTO public.student VALUES (6, 'Justyn', 'Kaleta', 'Mezczyzna', null);
+INSERT INTO public.student VALUES (7, 'Janina', 'Kaptur', 'Kobieta', null);
 
